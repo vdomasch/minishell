98b10d03185c701d6b1de6496606313d2dc12a4e
Vincent Domaschio <vdomasch@z1r2p3.42lyon.fr>
2024-04-05 15:53:59 +0200

    promt file

---
+++ sources/prompt.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdomasch <vdomasch@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 15:53:31 by vdomasch          #+#    #+#             */
+/*   Updated: 2024/04/05 15:53:32 by vdomasch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+void	ft_isterminal(void)
+{
+	int	fd;
+
+	fd = 0;
+	printf("isatty = %d\n", isatty(fd));	
+	printf("ttyname = %s\n", ttyname(fd));
+
+}
+
+char	*ft_getcwd(void)
+{
+	char *pwd;
+    
+    pwd = getcwd(NULL, 0);
+	if (pwd == NULL)
+	{
+		printf("Error pwd!\n");
+	}
+	return (pwd);
+}
+
+void	ft_readline(void)
+{
+	char	*pwd;
+    char	*rl;
+
+	pwd = ft_getcwd();
+	if (pwd == NULL)
+		return ;
+	printf("%s$ ", pwd);
+	free(pwd);
+    rl = readline("");
+	if (rl[0] == 'e' && rl[1] == 'x' && rl[2] == 'i' && rl[3] == 't' && rl[4] == '\0')
+	{
+		free(rl);
+		return ;
+	}
+	else if (rl[0] == 'p' && rl[1] == 'w' && rl[2] == 'd' && rl[3] == '\0')
+	{
+		pwd = ft_getcwd();
+		printf("%s\n", pwd);
+		free(pwd);
+	}
+	else if (!rl[0])
+		printf("\n");
+	else
+	{
+		printf("minishell: command not found: %s\n", rl);
+	}
+	free(rl);
+	ft_readline();
+}
--- sources/test.c
+++ sources/test.c
@@ -13,60 +13,6 @@
 #include "../includes/minishell.h"
 #include <string.h>
 
-void	ft_isterminal(void)
-{
-	int	fd;
-
-	fd = 0;
-	printf("isatty = %d\n", isatty(fd));	
-	printf("ttyname = %s\n", ttyname(fd));
-
-}
-
-char	*ft_getcwd(void)
-{
-	char *pwd;
-    
-    pwd = getcwd(NULL, 0);
-	if (pwd == NULL)
-	{
-		printf("Error pwd!\n");
-	}
-	return (pwd);
-}
-
-void	ft_readline(void)
-{
-	char	*pwd;
-    char	*rl;
-
-	pwd = ft_getcwd();
-	if (pwd == NULL)
-		return ;
-	printf("%s$ ", pwd);
-	free(pwd);
-    rl = readline("");
-	if (rl[0] == 'e' && rl[1] == 'x' && rl[2] == 'i' && rl[3] == 't' && rl[4] == '\0')
-	{
-		free(rl);
-		return ;
-	}
-	else if (rl[0] == 'p' && rl[1] == 'w' && rl[2] == 'd' && rl[3] == '\0')
-	{
-		pwd = ft_getcwd();
-		printf("%s\n", pwd);
-		free(pwd);
-	}
-	else if (!rl[0])
-		printf("\n");
-	else
-	{
-		printf("minishell: command not found: %s\n", rl);
-	}
-	free(rl);
-	ft_readline();
-}
-
 int main(void)
 {
 	//struct sigaction	act;
