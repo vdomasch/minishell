6ed8c41788ac1ca9c41e2d2a79d4ce92dd055a30
Benjamin Humeau <bhumeau@z2r6p4.42lyon.fr>
2024-04-08 16:22:59 +0200

    Added CLION config

---
+++ sources/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdomasch <vdomasch@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 11:35:41 by bhumeau           #+#    #+#             */
+/*   Updated: 2024/04/08 12:59:17 by vdomasch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int main(void)
+{
+    //struct sigaction	act;
+    //sigset_t			mask;
+
+    //sigemptyset(&mask);
+    //act.sa_mask = mask;
+    ft_readline();
+    ft_isterminal();
+    return (0);
+}
